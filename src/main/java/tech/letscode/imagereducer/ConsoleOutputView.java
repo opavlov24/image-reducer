1509a2476f88bb3e613127eeeb6019470f98e8ee
Oleg Pavlov <oleg.pavlov@aol.com>
2016-11-10 16:07:27 +0300

    Add ConsoleOutputView

---
+++ src/main/java/tech/letscode/imagereducer/ConsoleOutputView.java
@@ -0,0 +1,36 @@
+package tech.letscode.imagereducer;
+
+import javax.annotation.Nonnull;
+import java.io.File;
+import java.io.PrintStream;
+
+public class ConsoleOutputView implements OutputView
+{
+    private final PrintStream OUT = System.out;
+
+    private final PrintStream ERR = System.err;
+
+    @Override
+    public void outputFileCannotBeCreatedFor(@Nonnull File originalFile)
+    {
+        ERR.println("Output file cannot be created for original file - " + originalFile.getName());
+    }
+
+    @Override
+    public void originalImageCannotBeReduced(@Nonnull File originalFile)
+    {
+        ERR.println(originalFile + " - cannot be reduced");
+    }
+
+    @Override
+    public void unexpectedErrorHasBeenOccurred(@Nonnull Exception exception)
+    {
+        ERR.println("Unexpected error has been occurred. Message: " + exception.getMessage());
+    }
+
+    @Override
+    public void imageReduced(@Nonnull File originalFile)
+    {
+        OUT.println(originalFile.getName() + " has been reduced");
+    }
+}
