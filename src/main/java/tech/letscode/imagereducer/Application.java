5b14583d3a19f37156b526322aee086e580ac615
Oleg Pavlov <oleg.pavlov@aol.com>
2016-11-10 16:08:41 +0300

    Add logic of creating configuration and running the application

--- src/main/java/tech/letscode/imagereducer/Application.java
+++ src/main/java/tech/letscode/imagereducer/Application.java
@@ -2,8 +2,24 @@ package tech.letscode.imagereducer;
 
 public class Application
 {
+    private static final DirectoryScanner DIRECTORY_SCANNER = new SimpleDirectoryScanner();
+
+    private static final ImageReducer IMAGE_REDUCER = new SimpleImageReducer();
+
+    private static final OutputFileResolver OUTPUT_FILE_RESOLVER = new SimpleOutputFileResolver("compressed");
+
+    private static final OutputView OUTPUT_VIEW = new ConsoleOutputView();
+
     public static void main(String[] args)
     {
-        //TODO
+        ImageReducerConfiguration configuration = CommandLineConfiguration.build(args);
+        if (configuration != null) {
+            ImageReducerApplication application = new ImageReducerApplication(DIRECTORY_SCANNER,
+                                                                              IMAGE_REDUCER,
+                                                                              OUTPUT_FILE_RESOLVER,
+                                                                              configuration,
+                                                                              OUTPUT_VIEW);
+            application.run();
+        }
     }
 }
