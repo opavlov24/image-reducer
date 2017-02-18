9b48af1f75b0912fed118b9190670c4953e652e0
Oleg Pavlov <oleg.pavlov@aol.com>
2017-02-18 16:43:12 +0300

    Delete unused @NotNull

--- src/main/java/tech/letscode/imagereducer/exception/ImageReducerException.java
+++ src/main/java/tech/letscode/imagereducer/exception/ImageReducerException.java
@@ -1,12 +1,10 @@
 package tech.letscode.imagereducer.exception;
 
-import javax.annotation.Nonnull;
-
 public class ImageReducerException extends RuntimeException
 {
     private static final long serialVersionUID = -5154230166864416274L;
 
-    public ImageReducerException(@Nonnull String message, @Nonnull Throwable cause)
+    public ImageReducerException(String message, Throwable cause)
     {
         super(message, cause);
     }
--- src/main/java/tech/letscode/imagereducer/exception/OutputFileResolverException.java
+++ src/main/java/tech/letscode/imagereducer/exception/OutputFileResolverException.java
@@ -1,12 +1,10 @@
 package tech.letscode.imagereducer.exception;
 
-import javax.annotation.Nonnull;
-
 public class OutputFileResolverException extends RuntimeException
 {
     private static final long serialVersionUID = 4684298761821444653L;
 
-    public OutputFileResolverException(@Nonnull String message, @Nonnull Throwable cause)
+    public OutputFileResolverException(String message, Throwable cause)
     {
         super(message, cause);
     }
